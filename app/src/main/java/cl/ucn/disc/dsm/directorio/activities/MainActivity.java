f892a109eb64831bddfae62eefb134aa7d260a1e
AyleenOwO <ayleen.torres@alumnos.ucn.cl>
2018-11-30 00:22:48 -0300

    Update: Cargar archivos y la Barra de Busqueda

--- app/src/main/java/cl/ucn/disc/dsm/directorio/activities/MainActivity.java
+++ app/src/main/java/cl/ucn/disc/dsm/directorio/activities/MainActivity.java
@@ -7,34 +7,50 @@
 
 package cl.ucn.disc.dsm.directorio.activities;
 
-import android.app.ListActivity;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
-import android.widget.TextView;
 
-import com.google.android.material.snackbar.Snackbar;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
+
 import cl.ucn.disc.dsm.directorio.R;
-import cl.ucn.disc.dsm.directorio.adapters.PersonaAdapter;
-import lombok.extern.slf4j.Slf4j;
+import cl.ucn.disc.dsm.directorio.adapters.ListAdapter;
+import cl.ucn.disc.dsm.directorio.models.Persona;
 
 /**
- * Activity principal.
  *
- * @author Diego Urrutia-Astorga.
  */
-@Slf4j
-public class MainActivity extends ListActivity {
-    //extends AppCompatActivity
+public class MainActivity extends AppCompatActivity {
 
     /**
-     * Adaptador de Personas
+     *
      */
-    private PersonaAdapter personaAdapter;
+    private List<Persona> personas = new ArrayList<Persona>();
+
+    /**
+     *
+     */
+    private MaterialSearchView searchView;
+
+    /**
+     *
+     */
+    private ListView listView;
 
     /**
      *
@@ -44,62 +60,119 @@ public class MainActivity extends ListActivity {
                 AppCompatDelegate.MODE_NIGHT_AUTO);
     }
 
+
     /**
-     *
      * @param savedInstanceState
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
         // Toolbar
-        //Toolbar toolbar = findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        //ListView listView = findViewById(android.R.id.list);
+        try {
+            InputStream is = this.getAssets().open("ucn.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
 
-        //TextView empty = findViewById(android.R.id.empty);
-        //listView.setEmptyView(empty);
+            String json = new String(buffer, "UTF-8");
 
-        // Si el adaptador es null, no se ha construido
-        if (this.personaAdapter == null) {
+            JSONArray jsonArray = new JSONArray(json);
+            for (int i = 0; i < jsonArray.length(); i++) {
 
-            // .. lo contruyo ..
-            this.personaAdapter = new PersonaAdapter(this);
+                JSONObject object = jsonArray.getJSONObject(i);
+                personas.add(Persona.builder()
+                        .id(object.getString("id"))
+                        .nombre(object.getString("nombre"))
+                        .cargo(object.getString("cargo"))
+                        .unidad(object.getString("unidad"))
+                        .email(object.getString("email"))
+                        .telefono(object.getString("telefono"))
+                        .oficina(object.getString("oficina"))
+                        .build());
 
+            }
+
+        } catch (IOException exception) {
+            exception.printStackTrace();
+
+        } catch (JSONException exception) {
+            exception.printStackTrace();
         }
+        Collections.sort(this.personas, (p1, p2) -> p1.getNombre().compareTo(p2.getNombre()));
 
-        // Adaptador a utilizar para mostrar los datos
-        super.setListAdapter(this.personaAdapter);
+        searchView = (MaterialSearchView)findViewById(R.id.search_view);
+
+        listView = (ListView)findViewById(android.R.id.list);
+
+        ListAdapter adapter = new ListAdapter(this,R.layout.row_persona,personas);
+
+        listView.setAdapter(adapter);
+
+        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown() {
+
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+                ListView listView = (ListView)findViewById(android.R.id.list);
+
+                ListAdapter adapter = new ListAdapter(MainActivity.this,R.layout.row_persona,personas);
+
+                listView.setAdapter(adapter);
+
+            }
+
+        });
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if(newText != null && !newText.isEmpty()){
+                    List<Persona> lstFound = new ArrayList<>();
+                    for(Persona item:personas){
+                        if(item.getNombre().toLowerCase().contains(newText.toLowerCase())){
+                            lstFound.add(item);
+                        }
+                    }
+                    ListAdapter adapter = new ListAdapter(MainActivity.this,R.layout.row_persona,lstFound);
+                    listView.setAdapter(adapter);
+
+                }
+                else {
+                    ListAdapter adapter = new ListAdapter(MainActivity.this,R.layout.row_persona,personas);
+                    listView.setAdapter(adapter);
+                }
+                return true;
+            }
+        });
+
+
 
     }
 
+    /**
+     *
+     * @param menu
+     * @return
+     */
     @Override
-    protected void onStart() {
-        super.onStart();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_item,menu);
+        MenuItem item = menu.findItem(R.id.action_search);
+        searchView.setMenuItem(item);
+        return true;
 
-        // Si el adaptador no es null y no hay datos ..
-        if (this.personaAdapter != null && this.personaAdapter.isEmpty()) {
-
-            // Mensaje para mostrar que se estan cargando los datos
-            Snackbar.make(findViewById(android.R.id.content), "Loading data ..", Snackbar.LENGTH_LONG).show();
-
-            // Ejecuto en segundo plano ..
-            AsyncTask.execute(() -> {
-
-                log.debug("Loading data ..");
-                personaAdapter.load(this);
-
-                // .. ejecuto en el hilo principal y vuelve dibujarse
-                runOnUiThread(() -> {
-
-                    // Notifico que cambio el conjunto de datos y vuelve a dibujarlos
-                    personaAdapter.notifyDataSetChanged();
-
-                });
-
-            });
-        }
     }
 }
