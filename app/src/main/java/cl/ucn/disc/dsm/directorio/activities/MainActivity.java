32fe7b510f9fa30de536de50a1d67bc329499cba
AyleenOwO <ayleen.torres@alumnos.ucn.cl>
2018-12-01 17:15:45 -0300

    Dialog Persona

--- app/src/main/java/cl/ucn/disc/dsm/directorio/activities/MainActivity.java
+++ app/src/main/java/cl/ucn/disc/dsm/directorio/activities/MainActivity.java
@@ -1,16 +1,13 @@
-/*
- * Copyright (c) 2018.  Diego Urrutia Astorga <durrutia@ucn.cl>
- * This work is licensed under a Creative Commons Attribution-NonCommercial 4.0 International License.
- * http://creativecommons.org/licenses/by-nc/4.0/
- *
- */
 
 package cl.ucn.disc.dsm.directorio.activities;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
@@ -24,6 +21,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
@@ -73,8 +71,101 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        load();
+
+        searchView = (MaterialSearchView)findViewById(R.id.search_view);
+
+        listView = (ListView)findViewById(android.R.id.list);
+
+        ListAdapter adapter = new ListAdapter(this,R.layout.row_persona,personas);
+
+        clickItem(adapter, personas);
+
+        queryText();
+
+
+
+
+    }
+
+    /**
+     *
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_item,menu);
+        MenuItem item = menu.findItem(R.id.action_search);
+        searchView.setMenuItem(item);
+        return true;
+
+    }
+
+    private void clickItem(ListAdapter adapter, List<Persona> personas){
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+
+                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(MainActivity.this);
+                View dialogView = getLayoutInflater().inflate(R.layout.dialog_persona,null);
+
+                TextView nombre = (TextView)dialogView.findViewById(R.id.rp_tv_nombre);
+                TextView cargo = (TextView)dialogView.findViewById(R.id.rp_tv_cargo);
+                TextView unidad = (TextView)dialogView.findViewById(R.id.rp_tv_unidad);
+                TextView email = (TextView)dialogView.findViewById(R.id.rp_tv_email);
+                TextView telefono = (TextView)dialogView.findViewById(R.id.rp_tv_telefono);
+                TextView oficina = (TextView)dialogView.findViewById(R.id.rp_tv_oficina);
+
+                nombre.setText(personas.get(position).getNombre());
+                cargo.setText(personas.get(position).getCargo().equals("null") ? "" : personas.get(position).getCargo());
+                unidad.setText(personas.get(position).getUnidad().equals("null") ? "" : personas.get(position).getUnidad());
+                email.setText(personas.get(position).getEmail().equals("null") ? "" : personas.get(position).getEmail());
+                telefono.setText(personas.get(position).getTelefono().equals("null") ? "" : personas.get(position).getTelefono());
+                oficina.setText(personas.get(position).getOficina().equals("null") ? "" : personas.get(position).getOficina());
+
+                dialogBuilder.setView(dialogView);
+                dialogBuilder.create().show();
+            }
+        });
+
+    }
+
+    private void queryText(){
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if(newText != null && !newText.isEmpty()){
+                    List<Persona> lstFound = new ArrayList<>();
+                    for(Persona item:personas){
+                        if(item.getNombre().toLowerCase().contains(newText.toLowerCase())){
+                            lstFound.add(item);
+                        }
+                    }
+                    ListAdapter adapter = new ListAdapter(MainActivity.this,R.layout.row_persona,lstFound);
+                    clickItem(adapter, lstFound);
+
+                }
+                else {
+                    ListAdapter adapter = new ListAdapter(MainActivity.this,R.layout.row_persona,personas);
+                    clickItem(adapter, personas);
+                }
+                return true;
+            }
+        });
+
+    }
+
+    private void load(){
         try {
-            InputStream is = this.getAssets().open("ucn.json");
+            InputStream is = getAssets().open("ucn.json");
             int size = is.available();
             byte[] buffer = new byte[size];
             is.read(buffer);
@@ -105,74 +196,6 @@ public class MainActivity extends AppCompatActivity {
             exception.printStackTrace();
         }
         Collections.sort(this.personas, (p1, p2) -> p1.getNombre().compareTo(p2.getNombre()));
-
-        searchView = (MaterialSearchView)findViewById(R.id.search_view);
-
-        listView = (ListView)findViewById(android.R.id.list);
-
-        ListAdapter adapter = new ListAdapter(this,R.layout.row_persona,personas);
-
-        listView.setAdapter(adapter);
-
-        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
-            @Override
-            public void onSearchViewShown() {
-
-            }
-
-            @Override
-            public void onSearchViewClosed() {
-                ListView listView = (ListView)findViewById(android.R.id.list);
-
-                ListAdapter adapter = new ListAdapter(MainActivity.this,R.layout.row_persona,personas);
-
-                listView.setAdapter(adapter);
-
-            }
-
-        });
-        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                if(newText != null && !newText.isEmpty()){
-                    List<Persona> lstFound = new ArrayList<>();
-                    for(Persona item:personas){
-                        if(item.getNombre().toLowerCase().contains(newText.toLowerCase())){
-                            lstFound.add(item);
-                        }
-                    }
-                    ListAdapter adapter = new ListAdapter(MainActivity.this,R.layout.row_persona,lstFound);
-                    listView.setAdapter(adapter);
-
-                }
-                else {
-                    ListAdapter adapter = new ListAdapter(MainActivity.this,R.layout.row_persona,personas);
-                    listView.setAdapter(adapter);
-                }
-                return true;
-            }
-        });
-
-
-
     }
 
-    /**
-     *
-     * @param menu
-     * @return
-     */
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_item,menu);
-        MenuItem item = menu.findItem(R.id.action_search);
-        searchView.setMenuItem(item);
-        return true;
-
-    }
 }
